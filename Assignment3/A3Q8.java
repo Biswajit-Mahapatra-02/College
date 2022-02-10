b0f5fae76e0a326c27cb96828b9f8a0906223477
Biswajit-Mahapatra-02 <mahapatra.biswajit1102@gmail.com>
2022-02-10 09:19:20 +0530

    Assignment3
    College assignment 3

---
+++ Assignment3/A3Q8.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class A3Q8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int Rahul, Ayush, Ajay;
+		System.out.println("Entert the ages of Rahul,Ayush,Ajay respectively");
+		Rahul = sc.nextInt();
+		Ayush = sc.nextInt();
+		Ajay = sc.nextInt();
+		if (Rahul < Ayush && Rahul < Ajay) {
+			System.out.println("Youngest is Rahul");
+		} else if (Ayush < Rahul && Ayush < Ajay) {
+			System.out.println("The youngest is Ayush");
+		} else {
+			System.out.println("The youngest is Ajay");
+		}
+		sc.close();
+	}
+
+}
