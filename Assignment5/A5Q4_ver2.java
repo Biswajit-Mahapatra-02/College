9f190c848baa31f14d4698409d108ffccb8535c1
Biswajit-Mahapatra-02 <mahapatra.biswajit1102@gmail.com>
2022-02-14 12:45:27 +0530

    Assignment5 Question4 2 versions

---
+++ Assignment5/A5Q4_ver2.java
@@ -0,0 +1,39 @@
+package Assignment5;
+
+import java.util.Scanner;
+
+public class A5Q4_ver2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input;
+        int num_count=0;
+        int p_count=0;
+        int n_count=0;
+        int total=0;
+        int n;
+        double avg;
+        System.out.print("Enter numbers: ");
+        do{
+            n = sc.nextInt();
+            if(n==0)
+                break;
+            num_count++;
+            if(n>0)
+                p_count++;
+            else
+                n_count++;
+            total+=n;
+        }while(n!=0);
+        if(num_count>0) {
+            System.out.println("The number of positives is " + p_count);
+            System.out.println("The number of negatives is " + n_count);
+            System.out.println("The total is " + total);
+            avg = (double) total / num_count;
+            System.out.println("The average is " + avg);
+        }
+        else{
+            System.out.println("Enter an integer, the input ends if it is: 0");
+            System.out.println("No numbers are entered except 0");
+        }
+    }
+}
