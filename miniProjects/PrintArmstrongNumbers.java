12f6b876deb5e5f82e5ea565b0ef71597a19916e
Biswajit-Mahapatra-02 <mahapatra.biswajit1102@gmail.com>
2022-03-06 00:42:20 +0530

    Corrections

--- miniProjects/PrintArmstrongNumbers.java
+++ miniProjects/PrintArmstrongNumbers.java
@@ -4,9 +4,10 @@ public class PrintArmstrongNumbers {
    public static void main(String[] args) {
        int count=0;
         for(int i=0;i<=1000000;i++)
-            if(isArmstrongNumber(i))
-                count++;
-        System.out.println("Number of armstrong numbers between 0 and 1000000: "+count);
+            if(isArmstrongNumber(i)){
+                System.out.print(i+" ");
+                count++;}
+        System.out.println("\nNumber of armstrong numbers between 0 and 1000000: "+count);
    }
    public static boolean isArmstrongNumber(int n) {
        int dup = n,sum=0,d;
