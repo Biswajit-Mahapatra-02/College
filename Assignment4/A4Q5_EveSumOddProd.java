dbe81aa738fbab1c76f4b3a17d3577a84ac91b16
Biswajit-Mahapatra-02 <mahapatra.biswajit1102@gmail.com>
2022-03-05 21:04:51 +0530

    Update?

--- Assignment4/A4Q5_EveSumOddProd.java
+++ Assignment4/A4Q5_EveSumOddProd.java
@@ -7,12 +7,12 @@ public class A4Q5_EveSumOddProd {
         int product = 1;
         System.out.print("Enter range: ");
         int N = sc.nextInt();
-        for( int i = 1 ; i <= N; i++){
+        for( int i = 1 ; i <= N; i++)
             if (i%2 == 0)
                 sum+=i;
             else
                 product*=i;
-        }
+        
         System.out.println("Sum of all even numbers = "+sum);
         System.out.println("Product of all odd numbers = "+product);
         sc.close();
