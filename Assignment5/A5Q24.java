3eca7ca2082f5a11e228d3256c7020c8942e5f13
Biswajit-Mahapatra-02 <mahapatra.biswajit1102@gmail.com>
2022-03-09 12:52:38 +0530

    asdf

---
+++ Assignment5/A5Q24.java
@@ -0,0 +1,40 @@
+package Assignment5;
+
+import java.util.Scanner;
+
+public class A5Q24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int a = 1; a <= n; a++) {
+            int a3 = a*a*a;
+            if (a3 > n) break;
+
+            // start at a to avoid print out duplicate
+            for (int b = a; b <= n; b++) {
+                int b3 = b*b*b;
+                if (a3 + b3 > n) break;
+
+                // start at a + 1 to avoid printing out duplicates
+                for (int c = a + 1; c <= n; c++) {
+                    int c3 = c*c*c;
+                    if (c3 > a3 + b3) break;
+
+                    // start at c to avoid printing out duplicates
+                    for (int d = c; d <= n; d++) {
+                        int d3 = d*d*d;
+                        if (c3 + d3 > a3 + b3) break;
+
+                        if (c3 + d3 == a3 + b3) {
+                            System.out.print((a3+b3) + " = ");
+                            System.out.print(a + "^3 + " + b + "^3 = "); 
+                            System.out.print(c + "^3 + " + d + "^3"); 
+                            System.out.println();
+                        }
+                    }
+                }
+            }
+        }
+        sc.close();
+    }
+}
