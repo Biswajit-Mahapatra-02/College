a9ce06616d8ac9c9ac8633085d6864d7b5c79279
Biswajit Mahapatra <mahapatra.biswajit1102@gmail.com>
2022-03-17 09:54:53 +0000

    sdf

--- DM/testclass.java
+++ DM/testclass.java
@@ -4,6 +4,6 @@ public class testclass {
     public static void main(String[] args) {
         int num=645;
         System.out.println(Integer.toBinaryString(num));
-        System.out.println(12321%645);
+        System.out.println(3%99);
     }
 }
